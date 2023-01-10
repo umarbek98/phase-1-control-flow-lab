0ed6b7c7b2818f773200ff432d2497161a7a0fc8
Umarbek Rakhmatov <umarbek1306@gmail.com>
2023-01-09 22:33:52 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue <= 400) {
+    return 'This one is on me!'
+  } else if (someValue >= 400 && someValue <= 2000) {
+    return 'That will be twenty bucks.'
+  } else if (someValue > 2000 && someValue <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (someValue > 2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destLoc){
   // Write your code here!
+  return destLoc === 'NYC' ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip) {
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return "Thank you."
+      default: return "Bye.";
+  }
 }
+
+
+console.log(scuberGreetingForFeet(2501));
